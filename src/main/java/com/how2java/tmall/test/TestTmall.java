295b0866f63e4ec1894cb08986544f60eb45b3b8
zhoudb <zhoudb@asiainfo.com>
2018-03-04 21:46:44 +0800

    test2

--- src/main/java/com/how2java/tmall/test/TestTmall.java
+++ src/main/java/com/how2java/tmall/test/TestTmall.java
@@ -1,3 +1,7 @@
+/**
+ * 模仿天猫整站ssm 教程 为how2j.cn 版权所有 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关 供购买者学习，请勿私自传播，否则自行承担相关法律责任
+ */
+
 package com.how2java.tmall.test;
 
 import java.sql.Connection;
@@ -7,34 +11,40 @@ import java.sql.Statement;
 
 public class TestTmall {
 
-    public static void main(String args[]){
-        //准备分类测试数据：
+  public static void main(String args[]) {
+    //准备分类测试数据：
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try (
-                Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tmall_ssm?useUnicode=true&characterEncoding=utf8",
-                        "root", "root");
-                Statement s = c.createStatement();
-        )
-        {
-            for (int i = 1; i <=10 ; i++) {
-                String sqlFormat = "insert into category values (null, '测试分类%d')";
-                String sql = String.format(sqlFormat, i);
-                s.execute(sql);
-            }
-
-            System.out.println("已经成功创建10条分类测试数据");
-
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
     }
 
+    try (
+        Connection c = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/tmall_ssm?useUnicode=true&characterEncoding=utf8",
+            "root", "admin");
+        Statement s = c.createStatement();
+    ) {
+
+      s.execute("delete from category");
+      for (int i = 1; i <= 10; i++) {
+        String sqlFormat = "insert into category values (null, '测试分类%d')";
+        String sql = String.format(sqlFormat, i);
+        System.out.println(sql);
+        s.execute(sql);
+      }
+
+      System.out.println("已经成功创建10条分类测试数据");
+
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+  }
+
 }
+/**
+ * 模仿天猫整站ssm 教程 为how2j.cn 版权所有 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关 供购买者学习，请勿私自传播，否则自行承担相关法律责任
+ */
--- src/main/webapp/WEB-INF/jsp/admin/listCategory.jsp
+++ src/main/webapp/WEB-INF/jsp/admin/listCategory.jsp
@@ -1,21 +1,25 @@
+<!-- 模仿天猫整站ssm 教程 为how2j.cn 版权所有-->
+<!-- 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关-->
+<!-- 供购买者学习，请勿私自传播，否则自行承担相关法律责任-->
+
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-         pageEncoding="UTF-8" import="java.util.*"%>
+         pageEncoding="UTF-8" %>
 
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@include file="../include/admin/adminHeader.jsp"%>
 <%@include file="../include/admin/adminNavigator.jsp"%>
 
 <script>
-    $(function(){
+  $(function () {
 
-        $("#addForm").submit(function(){
-            if(!checkEmpty("name","分类名称"))
-                return false;
-            if(!checkEmpty("categoryPic","分类图片"))
-                return false;
-            return true;
-        });
+    $("#addForm").submit(function () {
+      if (!checkEmpty("name", "分类名称"))
+        return false;
+      if (!checkEmpty("categoryPic", "分类图片"))
+        return false;
+      return true;
     });
+  });
 
 </script>
 
@@ -59,7 +63,7 @@
     </div>
 
     <div class="pageDiv">
-        <%//@include file="../include/admin/adminPage.jsp" %>
+        <%@include file="../include/admin/adminPage.jsp" %>
     </div>
 
     <div class="panel panel-warning addDiv">
@@ -69,7 +73,7 @@
                 <table class="addTable">
                     <tr>
                         <td>分类名称</td>
-                        <td><input  id="name" name="name" type="text" class="form-control"></td>
+                        <td><input id="name" name="name" type="text" class="form-control"></td>
                     </tr>
                     <tr>
                         <td>分类圖片</td>
